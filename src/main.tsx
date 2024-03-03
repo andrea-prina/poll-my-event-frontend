fc410f133164d6feb0cfd0ff588c0602b600cc9a
Andrea Prina <andrea.prina@enpal.de>
2024-03-03 16:51:18 +0100

    redux

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import SignUp from './SignUp';
 import LogIn from './LogIn';
+import store from './app/store';
+import { Provider } from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
 
 const router = createBrowserRouter([
@@ -24,8 +26,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CookiesProvider>
-      <RouterProvider router={router} />
-    </CookiesProvider>
+    <Provider store={store}>
+      <CookiesProvider>
+        <RouterProvider router={router} />
+      </CookiesProvider>
+    </Provider>
   </React.StrictMode>
 );
